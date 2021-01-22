924d134fc3eb7cd13d28ff2d30d9497f28490fd1
Kimseongwon98 <72725317+Kimseongwon98@users.noreply.github.com>
2021-01-22 13:45:12 +0900

    commit on 'Master'

---
+++ app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import userRouter from "./routers/userRouter.js";
+import videoRouter from "./routers/videoRouter.js";
+import globalRouter from "./routers/globalRouter.js";
+import routes from "./routes.js";
+
+const app = express();
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
+
+export default app;
+
+/*respond with "hello world" when GET request is made to the homepage
+app.get("/", function (req, res) {
+  res.send("hello world");
+});*/
---
+++ routers/userRouter.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const userRouter = express.Router();
+
+export default userRouter;
+
+/* M data
+   V how dose tje data look
+   C function that looks for the data */
