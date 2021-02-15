f1126f3a7dfae2590453db34c8c9aa1f223956b5
Kimseongwon98 <72725317+Kimseongwon98@users.noreply.github.com>
2021-02-15 22:32:20 +0900

    commit on 'master'

--- static/main.js
+++ static/main.js
@@ -16,7 +16,17 @@
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scss_styles_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../scss/styles.scss */ \"./assets/scss/styles.scss\");\n\r\n\r\nconst something = async () => {\r\n  console.log(\"something\");\r\n};\r\n\n\n//# sourceURL=webpack://wetube/./assets/js/main.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scss_styles_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../scss/styles.scss */ \"./assets/scss/styles.scss\");\n/* harmony import */ var _videoPlayer_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./videoPlayer.js */ \"./assets/js/videoPlayer.js\");\n/* harmony import */ var _videoPlayer_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_videoPlayer_js__WEBPACK_IMPORTED_MODULE_1__);\n\r\n\r\n\r\n\n\n//# sourceURL=webpack://wetube/./assets/js/main.js?");
+
+/***/ }),
+
+/***/ "./assets/js/videoPlayer.js":
+/*!**********************************!*\
+  !*** ./assets/js/videoPlayer.js ***!
+  \**********************************/
+/***/ (() => {
+
+eval("const videoContainer = document.getElementById(\"jsVideoPlayer\");\r\nconst videoPlayer = document.querySelector(\"#jsVideoPlayer video\");\r\nconst playBtn = document.getElementById(\"jsPlayButton\");\r\nconst volumeBtn = document.getElementById(\"jsVolumeBtn\");\r\nconst fullScreenBtn = document.getElementById(\"jsFullScreen\");\r\nconst currentTime = document.getElementById(\"currentTime\");\r\nconst totalTime = document.getElementById(\"totalTime\");\r\nconst volumeRange = document.getElementById(\"jsVolume\");\r\n\r\nfunction handlePlayClick() {\r\n  if (videoPlayer.paused) {\r\n    videoPlayer.play();\r\n    playBtn.innerHTML = '<i class=\"fas fa-pause\"></i>';\r\n  } else {\r\n    videoPlayer.pause();\r\n    playBtn.innerHTML = '<i class=\"fas fa-play\"></i>';\r\n  }\r\n}\r\n\r\nfunction handleVolumeIcon(value) {\r\n  if (value >= 0.6) {\r\n    volumeBtn.innerHTML = '<i class=\"fas fa-volume-up\"></i>';\r\n  } else if (value >= 0.3) {\r\n    volumeBtn.innerHTML = '<i class=\"fas fa-volume-down\"></i>';\r\n  } else {\r\n    volumeBtn.innerHTML = '<i class=\"fas fa-volume-off\"></i>';\r\n  }\r\n}\r\n\r\nfunction handleVolumeClick() {\r\n  if (videoPlayer.muted) {\r\n    videoPlayer.muted = false;\r\n    volumeRange.value = videoPlayer.volume;\r\n    handleVolumeIcon(volumeRange.value);\r\n  } else {\r\n    volumeRange.value = 0;\r\n    videoPlayer.muted = true;\r\n    volumeBtn.innerHTML = '<i class=\"fas fa-volume-mute\"></i>';\r\n  }\r\n}\r\n\r\nfunction exitFullScreen() {\r\n  fullScreenBtn.innerHTML = '<i class=\"fas fa-expand\"></i>';\r\n  fullScreenBtn.removeEventListener(\"click\", exitFullScreen);\r\n  fullScreenBtn.addEventListener(\"click\", goFullScreen);\r\n  if (document.exitFullscreen) {\r\n    document.exitFullscreen();\r\n  } else if (document.mozCancelFullScreen) {\r\n    document.mozCancelFullScreen();\r\n  } else if (document.webkitExitFullscreen) {\r\n    document.webkitExitFullscreen();\r\n  } else if (document.msExitFullscreen) {\r\n    document.msExitFullscreen();\r\n  }\r\n  videoPlayer.classList.add(\"notFull\");\r\n}\r\n\r\nfunction goFullScreen() {\r\n  if (videoContainer.requestFullscreen) {\r\n    videoContainer.requestFullscreen();\r\n  } else if (videoContainer.mozRequestFullScreen) {\r\n    videoContainer.mozRequestFullScreen();\r\n  } else if (videoContainer.webkitRequestFullscreen) {\r\n    videoContainer.webkitRequestFullscreen();\r\n  } else if (videoContainer.msRequestFullscreen) {\r\n    videoContainer.msRequestFullscreen();\r\n  }\r\n  fullScreenBtn.innerHTML = '<i class=\"fas fa-compress\"></i>';\r\n  videoPlayer.classList.remove(\"notFull\");\r\n  fullScreenBtn.removeEventListener(\"click\", goFullScreen);\r\n  fullScreenBtn.addEventListener(\"click\", exitFullScreen);\r\n}\r\n\r\nconst formatDate = (seconds) => {\r\n  const secondsNumber = parseInt(seconds, 10);\r\n  let hours = Math.floor(secondsNumber / 3600);\r\n  let minutes = Math.floor((secondsNumber - hours * 3600) / 60);\r\n  let totalSeconds = secondsNumber - hours * 3600 - minutes * 60;\r\n\r\n  if (hours < 10) {\r\n    hours = `0${hours}`;\r\n  }\r\n  if (minutes < 10) {\r\n    minutes = `0${minutes}`;\r\n  }\r\n  if (seconds < 10) {\r\n    totalSeconds = `0${totalSeconds}`;\r\n  }\r\n  return `${hours}:${minutes}:${totalSeconds}`;\r\n};\r\n\r\nfunction getCurrentTime() {\r\n  currentTime.innerHTML = formatDate(Math.floor(videoPlayer.currentTime));\r\n}\r\n\r\nfunction setTotalTime() {\r\n  const totalTimeString = formatDate(videoPlayer.duration);\r\n  totalTime.innerHTML = totalTimeString;\r\n}\r\n\r\nfunction handleEnded() {\r\n  videoPlayer.currentTime = 0;\r\n  playBtn.innerHTML = '<i class=\"fas fa-play\"></i>';\r\n}\r\n\r\nfunction handleDrag(event) {\r\n  const {\r\n    target: { value },\r\n  } = event;\r\n  videoPlayer.volume = value;\r\n  handleVolumeIcon(value);\r\n}\r\n\r\nfunction init() {\r\n  videoPlayer.volume = 0.5;\r\n  playBtn.addEventListener(\"click\", handlePlayClick);\r\n  volumeBtn.addEventListener(\"click\", handleVolumeClick);\r\n  fullScreenBtn.addEventListener(\"click\", goFullScreen);\r\n  videoPlayer.addEventListener(\"loadedmetadata\", setTotalTime);\r\n  videoPlayer.addEventListener(\"timeupdate\", getCurrentTime);\r\n  videoPlayer.addEventListener(\"ended\", handleEnded);\r\n  volumeRange.addEventListener(\"input\", handleDrag);\r\n}\r\n\r\nif (videoContainer) {\r\n  init();\r\n}\r\n\n\n//# sourceURL=webpack://wetube/./assets/js/videoPlayer.js?");
 
 /***/ }),
 
@@ -3217,6 +3227,35 @@ eval("/**\n * Copyright (c) 2014-present, Facebook, Inc.\n *\n * This source cod
 /******/ 	}
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/make namespace object */
 /******/ 	(() => {
 /******/ 		// define __esModule on exports
